dbf2735f3ce627bdd4826adf36cab93e382b5802
Israel Romero <israelromero@ip-172-16-1-181.eu-west-1.compute.internal>
2023-04-27 10:46:39 +0100

    updating vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import basicSsl from "@vitejs/plugin-basic-ssl";
+// import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/opentok-react/",
-  plugins: [react(), basicSsl()],
-  // server: {
-  //   https: true,
-  // },
+  base: "/",
+  plugins: [react()],
 });
