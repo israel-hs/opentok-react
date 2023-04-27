e4e29e4123547dfd3973f1d4001e03f16edbd253
Israel Romero <israelromero@ip-172-16-1-181.eu-west-1.compute.internal>
2023-04-27 15:14:28 +0100

    reverting changes on session

--- src/lib/Video.tsx
+++ src/lib/Video.tsx
@@ -25,7 +25,7 @@ const Video: React.FC = () => {
   const [session, setSession] = useState<OT.Session>();
   const [publisher, setPublisher] = useState<OT.Publisher>();
   const [sessionStatus, setSessionStatus] = useState<Status>();
-  const [connectionStatus, setConnectionStatus] = useState<Status>();
+  // const [connectionStatus, setConnectionStatus] = useState<Status>();
 
   useEffect(() => {
     const initialSession = OT.initSession(apiKey, sessionId);
@@ -52,36 +52,36 @@ const Video: React.FC = () => {
 
     // Subscribe to a newly created stream
     let subscriber: any;
-    session.on({
-      streamCreated: (event: any) => {
-        subscriber = session.subscribe(
-          event.stream,
-          "subscriber",
-          videoProperties,
-          handleError
-        );
+    // session.on({
+    //   streamCreated: (event: any) => {
+    //     subscriber = session.subscribe(
+    //       event.stream,
+    //       "subscriber",
+    //       videoProperties,
+    //       handleError
+    //     );
 
-        subscriber.on({
-          disconnected: (_event: any) =>
-            setConnectionStatus(
-              "Stream has been disconnected unexpectedly. Attempting to automatically reconnect..."
-            ),
-          connected: (_event: any) => setConnectionStatus(undefined),
-        });
-      },
-    });
-    // session.on("streamCreated", function (event) {
-    //   subscriber = session.subscribe(
-    //     event.stream,
-    //     "subscriber",
-    //     {
-    //       insertMode: "append",
-    //       width: "100%",
-    //       height: "100%",
-    //     },
-    //     handleError
-    //   );
+    //     subscriber.on({
+    //       disconnected: (_event: any) =>
+    //         setConnectionStatus(
+    //           "Stream has been disconnected unexpectedly. Attempting to automatically reconnect..."
+    //         ),
+    //       connected: (_event: any) => setConnectionStatus(undefined),
+    //     });
+    //   },
     // });
+    session.on("streamCreated", function (event) {
+      subscriber = session.subscribe(
+        event.stream,
+        "subscriber",
+        {
+          insertMode: "append",
+          width: "100%",
+          height: "100%",
+        },
+        handleError
+      );
+    });
 
     session.on("streamDestroyed", function (event) {
       console.log("Stream " + event.stream.name + " ended. " + event.reason);
@@ -111,7 +111,7 @@ const Video: React.FC = () => {
         <div id={"subscriber"} ref={subscriberRef}></div>
         <div id={"publisher"} ref={publisherRef}></div>
       </div>
-      {!!connectionStatus && <div>{connectionStatus}</div>}
+      {/* {!!connectionStatus && <div>{connectionStatus}</div>} */}
     </>
   );
 };
