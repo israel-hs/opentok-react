4505551caf3fde2fbc3af9ecbd41208d0126cf67
Israel Romero <israelromero@ip-192-168-0-3.eu-west-1.compute.internal>
2023-06-28 21:59:34 +0100

    modify useOpentokSession to store the opentok credentials in the store

--- src/lib/hooks/useOpentokSession.tsx
+++ src/lib/hooks/useOpentokSession.tsx
@@ -1,19 +1,27 @@
 import OT from "@opentok/client";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 
 import { SignalEvent } from "../types";
-import { useEffect, useState } from "react";
 import { apiKey } from "../../opentok.config";
 import { getOpentokCredentials } from "../../api/callApi";
 import { createSessionListenersMap, handleError } from "../utils";
+import { AppDispatch, RootState } from "../../redux/store";
+import { setCredentials } from "../../redux/opentokCredentialsSlice";
 
 const useOpentokSession = () => {
   const [opentokSession, setOpentokSession] = useState<OT.Session>();
   const [signalText, setSignalText] = useState("");
   const [error, setError] = useState("");
 
-  useEffect(() => {
-    let session: OT.Session | undefined;
+  const dispatch: AppDispatch = useDispatch();
+  const { sessionId, token } = useSelector(
+    (state: RootState) => state.opentokCredentials
+  );
 
+  console.log({ sessionId, token });
+
+  useEffect(() => {
     const controller = new AbortController();
     const roomId = 1;
 
@@ -22,42 +30,48 @@ const useOpentokSession = () => {
         console.log({ credentials });
         const { openTokSessionId: sessionId, openTokAccessToken: token } =
           credentials;
-
-        console.log("session component is mounted");
-        session = OT.initSession(apiKey, sessionId);
-
-        session.connect(token, handleError);
-
-        const sessionEvents = createSessionListenersMap();
-
-        session.on({
-          ...sessionEvents,
-          // maybe this signal event should be moved to the Call component
-          "signal:therapist": (event: SignalEvent) => {
-            if (!session) return;
-            const sender = event.data;
-            const text =
-              event.from === session.connection
-                ? "You have emitted a signal"
-                : `You have received a signal ${
-                    sender ? `from ${sender}` : ""
-                  }`;
-
-            setSignalText(text);
-          },
-        });
-
-        setOpentokSession(session);
+        dispatch(setCredentials({ sessionId, token }));
       })
       .catch(() => setError("Error getting opentok credentials"));
 
     return () => {
       controller.abort();
-      session?.off(); // clean all session listeners
+    };
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (!sessionId || !token) return;
+
+    console.log("session component is mounted");
+    const session = OT.initSession(apiKey, sessionId);
+
+    session.connect(token, handleError);
+
+    const sessionEvents = createSessionListenersMap();
+
+    session.on({
+      ...sessionEvents,
+      // maybe this signal event should be moved to the Call component
+      "signal:therapist": (event: SignalEvent) => {
+        if (!session) return;
+        const sender = event.data;
+        const text =
+          event.from === session.connection
+            ? "You have emitted a signal"
+            : `You have received a signal ${sender ? `from ${sender}` : ""}`;
+
+        setSignalText(text);
+      },
+    });
+
+    setOpentokSession(session);
+
+    return () => {
+      session?.off();
       session?.disconnect();
       console.log("session disconnected");
     };
-  }, []);
+  }, [sessionId, token]);
 
   return { opentokSession, signalText, error };
 };
